ddc4e1ad6aab35d12db4457b78617abe8cba8d53
carlos solis p <solis.krlos@gmail.com>
2021-12-08 13:26:15 -0500

    8vo commit

--- SizeDirectoryFiles.psm1
+++ SizeDirectoryFiles.psm1
@@ -2,7 +2,7 @@
 .DESCRIPTION
 Muestra el tamaño de directorios y archivos en una Lista 
 .EXAMPLE
-C:\PS> SizeDir [Ocultos] 
+C:\PS> SizeDir [Ruta] 
 .NOTES
 Author: CSolisP
 Date:   Dic 2021
@@ -10,25 +10,18 @@ Date:   Dic 2021
 
 $global:TOTAL = @() 
 
+#funcion que halla el tamaño de un directorio 
 function size_recursivo 
 {
     [CmdletBinding()]
     param (
-        $RUTA = ".",
-        $Ocultados = ""
+        $Ruta = "." 
     )
  
     $ACUMULA = 0
 
-    if($Ocultados -eq "Ocultos")
-    {
-        $COMANDO = Get-ChildItem $RUTA -Force  
-    }
-    else 
-    {
-        $COMANDO = Get-ChildItem $RUTA 
-    }
-
+    $COMANDO = Get-ChildItem -Path $RUTA -Force  
+    
     foreach ($item in $COMANDO ) 
     {
         $FN = $item.FullName 
@@ -40,31 +33,109 @@ function size_recursivo
         else 
         {
             # Write-Host $FN 
-            $ACUMULA += (Get-Item $FN).Length 
+            $ACUMULA += (Get-Item -Path $FN -Force).Length 
         }  
     } 
     
-    $global:TOTAL  += $ACUMULA 
+    $global:TOTAL += $ACUMULA 
 }
 
-function SizeDir($Ocul = "") 
+#funcion para el tamanio total de directorio con subdirectorios y archivos 
+function SizeD($Ruta = ".") 
 {
     $SUMATOTAL = 0
 
-    if ($Ocul -eq "Ocultos") 
-    {
-        size_recursivo "." "Ocultos"
-    } 
-    else 
-    {
-        size_recursivo "." 
-    }
+    size_recursivo -Ruta $Ruta  
 
     foreach ($item in $global:TOTAL) 
     {
         $SUMATOTAL += $item 
     }
 
-    Write-Host "TOTAL = " $SUMATOTAL 
+    $SUMATOTAL 
     $global:TOTAL = @() 
 } 
+
+#funcion principal 
+function SizeDF 
+{
+    param (
+        $Ruta = "." 
+    )
+
+    Write-Host "`nTYPE        SIZE          NAME"
+    Write-Host "--------------------------------" 
+
+    $COMANDO = Get-ChildItem -Path $Ruta -Force 
+
+    $TOTALDIR = 0 
+    foreach ($item in $COMANDO )  
+    { 
+        $FN = $item.FullName  
+        
+        if(Test-Path -Path $FN -PathType Container) 
+        {
+            $TAMANIO = SizeD -Ruta $FN   
+            
+            #calculando tamanio
+            if($TAMANIO -lt 1024)
+            {
+                Write-Host -ForegroundColor Green  "dir."  ($TAMANIO/1).ToString("0.00").PadLeft(10)  "by `t"  $FN  
+            }
+            if($TAMANIO -ge 1024 -and $TAMANIO -lt 1048576) 
+            {
+                Write-Host -ForegroundColor Green  "dir."  ($TAMANIO/1kb).ToString("0.00").PadLeft(10)  "Kb `t"  $FN    
+            }
+            if($TAMANIO -ge 1048576 -and $TAMANIO -lt 1073741824) 
+            {
+                Write-Host -ForegroundColor Green  "dir."  ($TAMANIO/1mb).ToString("0.00").PadLeft(10)  "MB `t"  $FN  
+            }
+            if($TAMANIO -ge 1073741824) 
+            {
+                Write-Host -ForegroundColor Green  "dir."  ($TAMANIO/1gb).ToString("0.00").PadLeft(10)  "GB `t"  $FN  
+            }
+        } 
+        else 
+        {   
+            #calculando tamanio 
+            $TAMANIO = (Get-Item -Path $FN -Force).Length
+            
+            if($TAMANIO -lt 1024)
+            {
+                Write-Host  "file"  ($TAMANIO/1).ToString("0.00").PadLeft(10)  "by `t"  $FN  
+            }
+            if($TAMANIO -ge 1024 -and $TAMANIO -lt 1048576)
+            {
+                Write-Host  "file"  ($TAMANIO/1kb).ToString("0.00").PadLeft(10)  "Kb `t"  $FN  
+            }
+            if($TAMANIO -ge 1048576 -and $TAMANIO -lt 1073741824) 
+            {
+                Write-Host  "file"  ($TAMANIO/1mb).ToString("0.00").PadLeft(10)  "MB `t"  $FN  
+            }
+            if($TAMANIO -ge 1073741824) 
+            {
+                Write-Host  "file"  ($TAMANIO/1gb).ToString("0.00").PadLeft(10)  "GB `t"  $FN   
+            }
+        } 
+        ;
+        $TOTALDIR += $TAMANIO 
+    }
+    ; 
+    #mostrando resultado
+    if ($TOTALDIR -lt 2014) 
+    {
+        Write-Host  -ForegroundColor Green "`nTOTAL = "  ($TOTALDIR/1).ToString("0.00").PadLeft(5)  "by`n "     
+    }
+    if ($TOTALDIR -ge 1024 -and $TOTALDIR -lt 1048576) 
+    {
+        Write-Host  -ForegroundColor Green "`nTOTAL = "  ($TOTALDIR/1kb).ToString("0.00").PadLeft(5)  "Kb`n "     
+    }
+    if ($TOTALDIR -ge 1048576 -and $TOTALDIR -lt 1073741824)
+    {
+        Write-Host  -ForegroundColor Green "`nTOTAL = "  ($TOTALDIR/1mb).ToString("0.00").PadLeft(5)  "MB`n " 
+    }
+    if ($TOTALDIR -ge 1073741824)
+    {
+        Write-Host  -ForegroundColor Green "`nTOTAL = "  ($TOTALDIR/1gb).ToString("0.00").PadLeft(5)  "GB`n " 
+    }
+}
